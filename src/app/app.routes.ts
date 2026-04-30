22724247eb3f75c2085e6ecfcb24436c745a7f03
Federico Secco <federico.secco7@gmail.com>
2026-04-30 22:09:05 +0200

    Angular 21.2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
+  /* {
     title: 'Test',
     path: '',
     component: HomePage,
-  },
+  }, */
   {
     path: '**',
     redirectTo: '',
