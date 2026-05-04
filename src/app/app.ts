8ea2a7346e486c142d0cea6d8fa1da226ba7d969
Federico Secco <federico.secco7@gmail.com>
2026-05-04 23:26:27 +0200

    0.1.3
    Added app version on app-root

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import packageJson from '../../package.json';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+  host: {
+    '[attr.app-version]': 'appVersion',
+  },
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class App {}
+export class App {
+  protected readonly appVersion = packageJson.version;
+}
